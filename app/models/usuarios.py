18dbad17b1bfe44ead98e0ef71567f10ecc98ca4
Oliv-dev2908 <oliverradictos@gmail.com>
2025-09-29 22:56:59 -0400

    Apis para Usuarios
    Se rehicieron las apis para usuarios para que funcionen correctamente

--- app/models/usuarios.py
+++ app/models/usuarios.py
@@ -1,6 +1,9 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey
-from sqlalchemy.orm import relationship
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP
+from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.sql import func
+import uuid
+from sqlalchemy.orm import relationship
+
 from app.database import Base
 
 class Usuario(Base):
@@ -8,15 +11,12 @@ class Usuario(Base):
 
     id_usuario = Column(Integer, primary_key=True, index=True)
     nombre = Column(String, nullable=False)
-    email = Column(String, unique=True, index=True, nullable=False)
-    contraseña_hash = Column(String, nullable=False)
-    rol_id = Column(Integer, ForeignKey("roles.id_rol"), nullable=False)
+    email = Column(String, nullable=False, unique=True, index=True)
+    rol_id = Column(Integer, nullable=False)
     activo = Column(Boolean, default=True)
-    created_at = Column(DateTime(timezone=True), server_default=func.now())
-    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
-
-    # Relationships
-    # rol = relationship("Rol", back_populates="usuarios")
+    created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
+    updated_at = Column(TIMESTAMP(timezone=True), onupdate=func.now())
+    supabase_user_id = Column(UUID(as_uuid=True), default=uuid.uuid4)
     consultas = relationship("Consulta", back_populates="usuario")
     cirugias = relationship("Cirugia", back_populates="usuario")
     fisioterapias = relationship("Fisioterapia", back_populates="usuario")
--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -1,7 +1,8 @@
-from .usuarios import UsuarioBase, UsuarioCreate, UsuarioUpdate, UsuarioResponse, LoginRequest, Token
+from .usuarios import UsuarioBase, UsuarioCreate, UsuarioUpdate, UsuarioResponse
 # from .roles import RolBase, RolCreate, RolUpdate, RolResponse, PermisoBase, PermisoCreate, PermisoResponse
 from .clientes import ClienteBase, ClienteCreate, ClienteUpdate, ClienteResponse
 from .pacientes import PacienteBase, PacienteCreate, PacienteUpdate, PacienteResponse
 from .consultas import ConsultaBase, ConsultaCreate, ConsultaUpdate, ConsultaResponse
 from .constantes import ConstanteBase, ConstanteCreate, ConstanteUpdate, ConstanteResponse
 from .estudios import EstudioBase, EstudioCreate, EstudioUpdate, EstudioResponse
+
